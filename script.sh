5ac2dccd2e7a2fdcab984397bd6a738fe4bc22ac
Cloud User <ec2-user@ip-172-31-30-108.eu-north-1.compute.internal>
2024-09-26 12:05:10 +0000

    cron test 2024-09-26 12:05:10

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@ cd ~/training-biz-akshay
 
 export HISTTIMEFORMAT="%F %T "
 source ~/.bashrc
-history > commandslist.txt
+~/.bash_history > /home/ec2-user//training-biz-akshay/commandslist.txt
 
 git add .
 
