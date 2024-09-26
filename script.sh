98a004cbd9cabe2869ad7996f42159d035954868
Cloud User <ec2-user@ip-172-31-30-108.eu-north-1.compute.internal>
2024-09-26 12:45:44 +0000

    cron test 2024-09-26 12:45:44

--- script.sh
+++ script.sh
@@ -5,7 +5,5 @@ source ~/.bashrc
 cat ~/.bash_history > /home/ec2-user/training-biz-akshay/commandslist.txt
 
 git add .
-
 git commit -m "cron test `date +'%Y-%m-%d %H:%M:%S'`"
-
 git push origin main
